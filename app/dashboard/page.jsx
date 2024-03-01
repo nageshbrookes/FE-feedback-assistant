b899389fae163ca1d8e5eeb6199e4a2567000955
Benjamin Onyebuchi <benbyx@Benjamins-MacBook-Pro-2.local>
2024-03-01 13:14:53 +0100

    save file

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import EventCard from "@/components/EventCard";
+
 function page() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <EventCard />
+    </div>
+  );
 }
 
 export default page;
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,21 +4,22 @@ import linkedinIcon from "../public/images/linkedinIcon.png"
 import twitterIcon from "../public/images/twitterIcon.png"
 import youtubeIcon from "../public/images/youtubeIcon.png"
 import instagramIcon from "../public/images/instagramIcon.png"
+import Logo from "@components/image/logo";
 export default function(){
     return (
-        <div className={"flex justify-between items-center"}>
-            <div>© 2022 Company Name® Global Inc.</div>
+        <div className={"flex flex-col md:flex-row gap-6 justify-between items-center py-6"}>
+            <div>
+                <Logo/>
+            </div>
             <div className={"flex gap-2"}>
                 <Image src={facebookIcon} alt={""} width={"40"} height={"40"} />
                 <Image src={linkedinIcon} alt={""} />
                 <Image src={twitterIcon} alt={""} />
                 <Image src={youtubeIcon} alt={""} />
                 <Image src={instagramIcon} alt={""} />
-
             </div>
-            <div className={"flex gap-10"}>
-                <div>Terms of Service</div>
-                <div>Privacy Policy</div>
+            <div className={"text-xs md:text-base text-[#525252]"}>
+                © 2022 Company Name® Global Inc.
             </div>
         </div>
     )
