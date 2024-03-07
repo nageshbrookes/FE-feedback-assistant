2f470b6077c028135667e5637bd8ab89fa97332b
Naf1ssa <nafissa.siddique21@gmail.com>
2024-03-07 23:08:48 +0100

    added div

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Button({text, style, iconLeft, iconRight, children}) {
     return (
-        <button className={"transition-all duration-300 bg-opacity-80 hover:bg-opacity-100 " + style}>
-            {iconLeft}
-            {children ? children : text}
-            {iconRight}
-        </button>
+        <div>
+            <button className={"transition-all duration-300 bg-opacity-80 hover:bg-opacity-100 " + style}>
+                {iconLeft}
+                {children ? children : text}
+                {iconRight}
+            </button>
+        </div>
+
     );
 }
 
