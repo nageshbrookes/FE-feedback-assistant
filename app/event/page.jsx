4fb753994b7de20b305abf223ee99110d87c043b
Benjamin Onyebuchi <onyebuchy64@gmail.com>
2024-03-14 21:52:54 +0100

    Event table

--- app/event/page.jsx
+++ app/event/page.jsx
@@ -1,22 +1,95 @@
 import React from "react";
+import Image from "next/image";
+import ViewIcon from "../../public/images/viewicon.svg";
+import EventLinkIcon from "../../public/images/eventlinkicon.svg"
+import EditIcon from "../../public/images/editframeicon.svg";
+import DeleteIcon from "../../public/images/deleteicon.svg";
 
 function page() {
   return (
-    <div>
-      <h1>Event name</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel incidunt
-        sunt nisi, provident ipsam ratione architecto, distinctio, accusamus
-        obcaecati ipsum necessitatibus iste ullam. Ipsam enim, mollitia beatae
-        cum consectetur repellat ratione fugit iure fuga velit dolorum aliquid
-        laboriosam omnis, minima assumenda facere est adipisci excepturi placeat
-        suscipit! Aliquam molestias aut eos explicabo ea, odit corporis.
-        Repellendus laboriosam possimus obcaecati modi. Sapiente fugiat
-        repudiandae exercitationem dolorum saepe, a ab placeat cumque molestiae
-        illo optio id perferendis commodi vero incidunt adipisci magni voluptas
-        veritatis repellat autem temporibus ex corrupti natus suscipit. Quaerat
-        delectus minima quisquam adipisci dolores autem omnis modi, ea nostrum!
-      </p>
+
+    <div className={"py-8 px-[100px]"}>
+
+      {/*insert breadcrumb*/}
+      <div className={"flex gap-6"}>
+        <div>Home</div>
+        <div>/</div>
+        <div>Events</div>
+      </div>
+
+      {/* Creating Table */}
+      <div className={"bg-white"}>
+
+        <div> {/* Header */}
+
+        </div>
+
+
+        <table className={""}>
+          {/* Row 1 */}
+          <tr>
+            <th><input type="checkbox"></input></th>
+            <th>Event Name</th>
+            <th>Ticket Selling Period</th>
+            <th>Ticket Price</th>
+            <th>Action</th>
+          </tr>
+
+          {/* Row 2 */}
+          <tr>
+            <td>14587</td>
+            <td>JKT 48 11th Anniversary Concert</td>
+            <td>Sep 1, 2023 - Dec 1, 2023</td>
+            <td>25467</td>
+            <td>£110</td>
+            <td>
+              <div className={"flex"}>
+                <Image src={ViewIcon} alt={""} />
+                <Image src={EventLinkIcon} alt={""} />
+                <Image src={EditIcon} alt={""} />
+                <Image src={DeleteIcon} alt={""} />
+              </div>
+            </td>
+          </tr>
+
+          {/* Row 3 */}
+          <tr>
+            <td>14587</td>
+            <td>Coldplay Concert - From The Ground to The Sky</td>
+            <td>Sep 1, 2023 - Dec 1, 2023</td>
+            <td>20480</td>
+            <td>£115</td>
+            <td>
+              <div className={"flex"}>
+                <Image src={ViewIcon} alt={""} />
+                <Image src={EventLinkIcon} alt={""} />
+                <Image src={EditIcon} alt={""} />
+                <Image src={DeleteIcon} alt={""} />
+              </div>
+            </td>
+          </tr>
+
+          {/* Row 4 */}
+          <tr>
+            <td>14587</td>
+            <td>Neck Deep - From The Deep and More to Deep</td>
+            <td>Sep 1, 2023 - Dec 1, 2023</td>
+            <td>18188</td>
+            <td>£94</td>
+            <td>
+              <div className={"flex"}>
+                <Image src={ViewIcon} alt={""} />
+                <Image src={EventLinkIcon} alt={""} />
+                <Image src={EditIcon} alt={""} />
+                <Image src={DeleteIcon} alt={""} />
+              </div>
+            </td>
+          </tr>
+
+        </table>
+
+      </div>
+
     </div>
   );
 }
